0265bc8810805b69da7095ef5a1365b760a6342e
Okta Liem <surya_diharja@yahoo.co.id>
2019-11-24 21:57:35 +1300

    add web action library
    pure page object vs page factory

--- src/test/java/net/oktaliem/gauge/PageActions.java
+++ src/test/java/net/oktaliem/gauge/PageActions.java
@@ -1,27 +1,85 @@
+/**
+ * @Author Okta Liem
+ */
 package net.oktaliem.gauge;
 
 import net.continuumsecurity.proxy.Spider;
 import net.continuumsecurity.proxy.ZAProxyScanner;
 import net.oktaliem.utils.driver.Driver;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.zaproxy.clientapi.core.Alert;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static net.oktaliem.Path.LOGOUT_URL;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+
 public class PageActions extends Driver {
+
+    /**
+     * UI Elements consist of
+     * - Button -> clickOn
+     * - TextBox -> inputTextBox
+     * - Drop Down List -> selectDropDownListByText, selectOnDropDownListByValue
+     * - Radio Button -> selectRadioButtonByText,selectRadioButtonByValue
+     * - Check Box -> selectCheckBox
+     * - Text -> getTextFromElement
+     */
+
+    /**
+     * Verification consist of
+     *  - checkIfElementIsVisible
+     *  - checkIfTextIsExpected
+     */
+
+    /**
+     * Wait consist of
+     * - Wait
+     * - waitUntilElementIsVisible
+     */
+
+    /**
+     * General Actions consist of
+     * - refreshPage
+     * - goBack
+     * - goForward
+     * - getHtmlSource
+     * - clickViaJavascript
+     * - openNewTab
+     * - switchToSecondBrowserTab
+     * - switchToFirstBrowserTab
+     * - getValueWithRegex
+     * - uploadFile
+     * - getCurrentURL
+     * - handleJavascriptPopUp
+     */
+
 
     public PageActions() {
         super();
     }
+
+    /**
+     * Pure Page Object for Page Actions
+     */
 
     public void clickOn(By el) {
         driver.findElement(el).click();
@@ -33,15 +91,271 @@ public class PageActions extends Driver {
         log.info("User inputs field with element: " + el + " and value " + value);
     }
 
+    public String getTextFromElement(By el) {
+        String text = driver.findElement(el).getText();
+        log.info("Get Text with value: " + text);
+        return text;
+
+    }
+
+    public void selectOnDropDownListByText(By el, String text) {
+        WebElement element = driver.findElement(el);
+        Select select = new Select(element);
+        select.selectByVisibleText(text);
+        log.info("Select Drop down List Element by visible text : " + element);
+    }
+
+    public void selectOnDropDownListByValue(By el, String value) {
+        WebElement element = driver.findElement(el);
+        Select select = new Select(element);
+        select.selectByValue(value);
+        log.info("Select Drop down List Element by visible text : " + element);
+    }
+
+
+    public void selectOnRadioButtonByText(By els, String text) {
+        for (WebElement element : driver.findElements(els)) {
+            if (element.getText().equals(text)) {
+                element.click();
+                log.info("Select radio button by text: " + text);
+                break;
+            }
+        }
+    }
+
+    public void selectOnRadioButtonByValue(By els, String text) {
+        for (WebElement element : driver.findElements(els)) {
+            if (element.getAttribute("value").trim().equals(text.trim())) {
+                element.click();
+                log.info("Select radio button by value: " + text);
+                break;
+            }
+        }
+    }
+
+    public void selectCheckBox(By el, String status) {
+        WebElement element = driver.findElement(el);
+        if (status.equals("n")) {
+            if (element.isSelected()) {
+                element.click();
+            } else {
+                log.info("check box is disabled by default");
+            }
+        }
+        if (status.equals("y")) {
+            if (element.isSelected()) {
+                log.info("check box is already enabled");
+            } else {
+                element.click();
+            }
+        }
+    }
+
+
+
+    /**
+     * Page Factory for Page Actions
+     */
+
+    public void clickOn(WebElement element) {
+        element.click();
+        log.info("User clicks On Element: " + element);
+    }
+
+    public void inputTextBox(WebElement element, String value) {
+        element.sendKeys(value);
+        log.info("User inputs field with element: " + element + " and value " + value);
+    }
+
+    public String getTextFromElement(WebElement element) {
+        String text = element.getText();
+        log.info("Get Text with value: " + text);
+        return text;
+
+    }
+
+    public void selectOnDropDownListByText(WebElement element, String text) {
+        Select select = new Select(element);
+        select.selectByVisibleText(text);
+        log.info("Select Drop down List Element by visible text : " + element);
+    }
+
+    public void selectOnDropDownListByValue(WebElement element, String value) {
+        Select select = new Select(element);
+        select.selectByValue(value);
+        log.info("Select Drop down List Element by visible text : " + element);
+    }
+
+    public void selectOnRadioButtonByText(List<WebElement> elements, String text) {
+        for (WebElement element : elements) {
+            if (element.getText().trim().equals(text.trim())) {
+                element.click();
+                log.info("Select radio button by text: " + text);
+                break;
+            }
+        }
+    }
+
+    public void selectOnRadioButtonByValue(List<WebElement> elements, String text) {
+        for (WebElement element : elements) {
+            if (element.getAttribute("value").trim().equals(text.trim())) {
+                element.click();
+                log.info("Select radio button by value: " + text);
+                break;
+            }
+        }
+    }
+
+    public void selectCheckBox(WebElement element, String status) {
+        if (status.equals("n")) {
+            if (element.isSelected()) {
+                element.click();
+            } else {
+                log.info("check box is disabled by default");
+            }
+        }
+        if (status.equals("y")) {
+            if (element.isSelected()) {
+                log.info("check box is already enabled");
+            } else {
+                element.click();
+            }
+        }
+    }
+
+
+    /**
+     * Verification
+     */
+
+    public boolean checkIfElementIsVisible(WebElement element) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 3);
+            wait.until(ExpectedConditions.visibilityOf(element));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void checkIfTextIsExpected(WebElement element, String expected) {
+        Assert.assertEquals(element.getText(), expected);
+        log.info("Text is expected");
+    }
+
+
+    /**
+     * Wait actions
+     */
 
     public void wait(int milisecond) {
         try {
-            Thread.sleep(milisecond);
+            //Thread.sleep(milisecond);
+            TimeUnit.MILLISECONDS.sleep(milisecond);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         log.info("User waits for " + milisecond + " milliseconds");
     }
+
+    public void waitUntilElementIsVisible(WebElement el, int time) {
+        WebDriverWait wait = new WebDriverWait(driver, time);
+        wait.until(ExpectedConditions.visibilityOf(el));
+    }
+
+
+    /**
+     * General Actions
+     */
+
+    public void refreshPage() {
+        driver.navigate().refresh();
+    }
+
+    public void goBack() {
+        driver.navigate().back();
+    }
+
+    public void goForward() {
+        driver.navigate().forward();
+    }
+
+    public void getHtmlSource(String fileName) throws IOException {
+        String getActualFile = driver.getPageSource();
+        File DestFile = new File(System.getProperty("user.dir") + "/src/main/resources/actualhtmltext/"
+                + fileName + ".txt");
+        FileUtils.writeStringToFile(DestFile, getActualFile);
+        log.info("Get HTML resource succeed");
+    }
+
+    public void clickViaJavascript(WebElement el) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", el);
+        log.info("click on " + el + " via javascript");
+    }
+
+    public void openNewTab() {
+        ((JavascriptExecutor) driver).executeScript("window.open('about:blank','_blank');");
+        log.info("Open New Tab");
+    }
+
+    public void switchToSecondBrowserTab() {
+        ArrayList<String> tabs = new ArrayList(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        log.info("Go to second tab");
+    }
+
+    public void switchToFirstBrowserTab() {
+        ArrayList<String> tabs = new ArrayList(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(0));
+        log.info("Go to first tab");
+    }
+
+    public String getValueWithRegex(String regex, String text) {
+        String regexPattern = regex;
+        Pattern p = Pattern.compile(regexPattern);
+        Matcher m = p.matcher(text);
+        String validationCode = "";
+        if (m.find()) {
+            validationCode = m.group(0).trim();
+        }
+        log.info("Get Value with Regex " + regex + " and result: " + validationCode);
+        return validationCode;
+    }
+
+    public void uploadFile(WebElement element, String fileName) {
+        element.sendKeys(System.getProperty("user.dir") + "/src/main/resources/" + fileName);
+        log.info("Choose file name: " + fileName);
+    }
+
+    public String getCurrentURL() {
+        String url = driver.getCurrentUrl();
+        log.info("Current URL is " + url);
+        return url;
+    }
+
+    public void handleJavascriptPopUp(String info) {
+        try {
+            org.openqa.selenium.Alert alert = driver.switchTo().alert();
+            if (alert.getText().equals(info)) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+        } catch (Exception e) {
+            ((JavascriptExecutor) driver).executeScript("window.confirm = function() { return true; }");
+            ((JavascriptExecutor) driver).executeScript("window.alert = function() { return true; }");
+            ((JavascriptExecutor) driver).executeScript("window.prompt = function() { return true; }");
+        }
+        log.info("accepting javascript Pop Up");
+
+    }
+
+
+    /**
+     * OWASP ZAP Actions
+     */
+
 
     public void spiderWithZAP(String targetURL) {
         zapSetSetUp();
@@ -67,14 +381,14 @@ public class PageActions extends Driver {
         log.info("Spider Odoo done.....");
     }
 
-    private void zapSetSetUp(){
+    private void zapSetSetUp() {
         zapScanner = new ZAProxyScanner(ZAP_PROXYHOST, ZAP_PROXYPORT, ZAP_APIKEY);
         zapScanner.clear(); //Start a new session
         zapSpider = (Spider) zapScanner;
         log.info("Created client to ZAP API");
     }
 
-    public void setAlertAttackStrength(){
+    public void setAlertAttackStrength() {
         for (String policyName : policyNames) {
             String ids = enableZapPolicy(policyName);
             for (String id : ids.split(",")) {
@@ -159,12 +473,12 @@ public class PageActions extends Driver {
         return scannerIds;
     }
 
-    protected void setEnablePassiveScanner(boolean status){
+    protected void setEnablePassiveScanner(boolean status) {
         zapScanner.setEnablePassiveScan(status);
         log.info("Set Enable Passive Scanner ");
     }
 
-    protected void scanAppWithZAP(String url){
+    protected void scanAppWithZAP(String url) {
         log.info("Scanning..." + url);
         zapScanner.scan(url);
         currentScanID = zapScanner.getLastScannerScanId();
@@ -198,7 +512,7 @@ public class PageActions extends Driver {
     }
 
 
-    protected  void removeFalsePositive(){
+    protected void removeFalsePositive() {
         List<Alert> alerts = filterAlerts(zapScanner.getAlerts());
         logAlerts(alerts);
         assertThat(alerts.size(), equalTo(0));
